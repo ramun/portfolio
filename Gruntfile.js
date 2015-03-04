268f018bd087738a650cee99fcc3ed8ecf9fbff7
Ramun <ramunliesch@gmail.com>
2015-03-03 23:47:42 -0300

    further development and some text added

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,6 +155,7 @@ module.exports = function(grunt) {
         files: {
           '<%= config.dist %>/index.html': ['<%= config.src %>/templates/pages/index.hbs'],
           '<%= config.dist %>/project-localina.html': ['<%= config.src %>/templates/pages/project-localina.hbs'],
+          '<%= config.dist %>/cv.html': ['<%= config.src %>/templates/pages/cv.hbs'],
           '<%= config.dist %>/404.html': ['<%= config.src %>/templates/pages/404.hbs'],
         }
       }
